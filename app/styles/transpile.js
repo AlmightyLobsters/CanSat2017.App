b76327340d1d4201121c6961fd2ef52df5daa830
Vojtěch Štěpančík <Vojtch tpank>
2017-02-19 01:36:21 +0100

    Sass support - integration with browsersync

---
+++ app/styles/transpile.js
@@ -0,0 +1,19 @@
+const render = require('node-sass').render;
+const join = require('path').join;
+const fs = require('fs');
+
+module.exports = function transpile(cb) {
+    render({
+        file: join(__dirname, './main.scss'),
+        outputStyle: 'compressed'
+    }, (err, result) => {
+        if(err) console.log(err);
+        else {
+            if (!fs.existsSync(join(__dirname, '../../build/styles'))) fs.mkdirSync(join(__dirname, '../../build/styles'));
+            fs.writeFile(join(__dirname, '../../build/styles/main.css'), result.css, err => {
+                if (err) console.error(err);
+                if (cb) cb();
+            });
+        }
+    });
+};
--- scripts/serve.js
+++ scripts/serve.js
@@ -2,51 +2,60 @@ import { spawn } from 'child_process';
 import electron from 'electron';
 import browserSync from 'browser-sync';
 import browserSyncConnectUtils from 'browser-sync/lib/connect-utils';
+import transpile from '../app/styles/transpile';
 
 const bsync = browserSync.create();
 
 const getRootUrl = (options) => {
-  const port = options.get('port');
-  return `http://localhost:${port}`;
+    const port = options.get('port');
+    return `http://localhost:${port}`;
 };
 
 const getClientUrl = (options) => {
-  const pathname = browserSyncConnectUtils.clientScript(options);
-  return getRootUrl(options) + pathname;
+    const pathname = browserSyncConnectUtils.clientScript(options);
+    return getRootUrl(options) + pathname;
 };
 
 bsync.init({
-  ui: false,
-  // Port 35829 = LiveReload's default port 35729 + 100.
-  // If the port is occupied, Browsersync uses next free port automatically.
-  port: 35829,
-  ghostMode: false,
-  open: false,
-  notify: false,
-  logSnippet: false,
-  socket: {
-    // Use the actual port here.
-    domain: getRootUrl
-  }
+    ui: false,
+    // Port 35829 = LiveReload's default port 35729 + 100.
+    // If the port is occupied, Browsersync uses next free port automatically.
+    port: 35829,
+    ghostMode: false,
+    open: false,
+    notify: false,
+    logSnippet: false,
+    socket: {
+        // Use the actual port here.
+        domain: getRootUrl
+    }
 }, (err, bs) => {
-  if (err) return console.error(err);
+    if (err) return console.error(err);
 
-  const child = spawn(electron, ['.'], {
-    env: {
-      ...{
-        NODE_ENV: 'development',
-        BROWSER_SYNC_CLIENT_URL: getClientUrl(bs.options)
-      },
-      ...process.env
-    },
-    stdio: 'inherit'
-  });
+    const child = spawn(electron, ['.'], {
+        env: {
+            ... {
+                NODE_ENV: 'development',
+                BROWSER_SYNC_CLIENT_URL: getClientUrl(bs.options)
+            },
+            ...process.env
+        },
+        stdio: 'inherit'
+    });
 
-  child.on('close', () => {
-    process.exit();
-  });
+    child.on('close', () => {
+        process.exit();
+    });
 
-  bsync
-    .watch('build/**/*')
-    .on('change', bsync.reload);
+    bsync
+        .watch('build/**/*')
+        .on('change', bsync.reload);
+
+    bsync
+        .watch('public/**/*')
+        .on('change', bsync.reload);
+
+    bsync
+        .watch('app/styles/**/*')
+        .on('change', () => transpile(bsync.reload));
 });
