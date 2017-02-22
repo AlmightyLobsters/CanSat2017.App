038c3635a24e86ceb5c164332e654c039342ebf1
vojtechstepancik@outlook.com <--get>
2017-02-22 18:00:09 +0100

    Update active sidebar element on navigation

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,36 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
 
 const toggleSidepanel = () => {
     document.getElementById('sidebar').classList.toggle('expanded');
     document.getElementById('detail').classList.toggle('expanded');
 };
 
-const Sidebar = _ => (
+const activeIf = (value, assumption) => value === assumption ? ' active' : '';
+
+const Sidebar = ({location}) => (
     <div id="sidebar" className="expanded">
         <ul className="navbar">
             <section className="top">
                 <li>
                     <a style={{fontFamily: 'Novecento Bold'}} onClick={e => toggleSidepanel()}><i className="fa fa-bars fa-fw fa-2x"></i><label>Marigold</label></a>
                 </li>
-                <li className="active">
+                <li className={activeIf(location, '/')}>
                     <Link to="/"><i className="fa fa-tachometer fa-fw fa-2x"></i><label>Dashboard</label></Link>
                 </li>
-                <li>
+                <li className={activeIf(location, '/map')}>
                     <Link to="/map"><i className="fa fa-map fa-fw fa-2x"></i><label>Map</label></Link>
                 </li>
-                <li>
+                <li className={activeIf(location, '/graphs')}>
                     <Link to="/graphs"><i className="fa fa-area-chart fa-fw fa-2x"></i><label>Graphs</label></Link>
                 </li>
-                <li>
+                <li className={activeIf(location, '/gallery')}>
                     <Link to="/gallery"><i className="fa fa-camera fa-fw fa-2x"></i><label>Gallery</label></Link>
                 </li>
-                <li>
+                <li className={activeIf(location, '/terminal')}>
                     <Link to="/terminal"><i className="fa fa-terminal fa-fw fa-2x"></i><label>Terminal</label></Link>
                 </li>
             </section>
             <section className="bottom">
-                <li>
+                <li className={activeIf(location, '/settings')}>
                     <Link to="/settings"><i className="fa fa-cog fa-fw fa-2x"></i><label>Settings</label></Link>
                 </li>
             </section>
@@ -38,4 +41,6 @@ const Sidebar = _ => (
     </div>
 );
 
-export default Sidebar;
+export default connect(store => ({
+    location: store.routing.locationBeforeTransitions.pathname
+}))(Sidebar);
