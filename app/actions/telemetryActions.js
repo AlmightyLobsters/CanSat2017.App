90a2ea0e1560b8dc08ac195bd6c52800e5786748
VojtechStep <vojtechstepancik@outlook.com>
2017-03-17 16:59:32 +0100

    Add telemetry Redux action creators

---
+++ app/actions/telemetryActions.js
@@ -0,0 +1,80 @@
+import * as types from './telemetryActionTypes';
+
+/*
+    Packet structure:
+    TIME,TEMPERATURE,PRESSURE,HUMIDITY,LATITUDE,LATITUDE_ORIENTATION,LONGITUDE,LONGITUDE_ORIENTATION,ALTITUDE,RSSI,BATTERY_LEVEL,PROPELLER_ROTATION,X_ACCELERATION,Y_ACCELERATION,Z_ACCELERATION,X_ROTATION,Y_ROTATION,Z_ROTATION,X_MAGNETIC_FIELD,Y_MAGNETIC_FIELD,Z_MAGNETIC_FIELD
+*/
+
+export const addTimeAction = time => ({
+    type: types.ADD_TIME,
+    payload: time
+});
+
+export const addPrimAction = (temperature, pressure, humidity) => ({
+    type: types.ADD_PRIM,
+    payload: {
+        temperature,
+        pressure,
+        humidity
+    }
+});
+
+export const addGpsAction = (latitudeVal, latitudeOr, longitudeVal, longitudeOr, altitudeVal) => ({
+    type: types.ADD_GPS,
+    payload: {
+        latitude: {
+            value: latitudeVal,
+            orientation: latitudeOr
+        },
+        longitude: {
+            value: longitudeVal,
+            orientation: longitudeOr
+        },
+        altitude: altitudeVal
+    }
+});
+
+export const addStatsAction = (rssi, batteryLevel, propellerRotations) => ({
+    type: types.ADD_STATS,
+    payload: {
+        rssi,
+        batteryLevel,
+        propellerRotations
+    }
+});
+
+export const addAccAction = (x, y, z) => ({
+    type: types.ADD_ACC,
+    payload: {
+        x,
+        y,
+        z
+    }
+});
+
+export const addRotAction = (x, y, z) => ({
+    type: types.ADD_ROT,
+    payload: {
+        x,
+        y,
+        z
+    }
+});
+
+export const addMagAction = (x, y, z) => ({
+    type: types.ADD_MAG,
+    payload: {
+        x,
+        y,
+        z
+    }
+});
+
+export const addVelAction = (accelerationVelocity, altitudeVelocity, pressureVelocity) => ({
+    type: types.ADD_VEL,
+    payload: {
+        accelerationVelocity,
+        altitudeVelocity,
+        pressureVelocity
+    }
+});
