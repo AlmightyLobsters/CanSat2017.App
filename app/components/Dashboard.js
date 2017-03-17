3cf4bc9d471d52bce0d0205f5a6cf60144672975
VojtechStep <vojtechstepancik@outlook.com>
2017-03-17 17:02:33 +0100

    Add testing button to Dashboard

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { addPacket } from '../helpers';
+
+const ex_packet = '5,16.8,997,420,50,N,60,E,4,-55,3,2000,0.5,0,1,2,4,8,16,32,64';
 
 const Dashboard = _ => (
     <div id="dashboard">
+        <button onClick={e => addPacket(ex_packet)}>Generate</button>
         <section className="AltWrapper">
             <div className="Altitude">
                 <h1 id="RelativeAltitude">467m</h1>
