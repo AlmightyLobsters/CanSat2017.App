de375795e96a7ee1e933f1d802d5e1cfbefbcb33
VojtechStep <vojtechstepancik@outlook.com>
2017-03-17 16:58:50 +0100

    Add telemetry Redux action types

---
+++ app/actions/telemetryActionTypes.js
@@ -0,0 +1,8 @@
+export const ADD_TIME = 'ADD_TIME';
+export const ADD_PRIM = 'ADD_PRIM';
+export const ADD_GPS = 'ADD_GPS';
+export const ADD_STATS = 'ADD_STATS';
+export const ADD_ACC = 'ADD_ACC';
+export const ADD_ROT = 'ADD_ROT';
+export const ADD_MAG = 'ADD_MAG';
+export const ADD_VEL = 'ADD_VEL';
