0a67e3224c9fd558b04690b5df2eed739d2b75d6
James Bohacek <jamesbohacek@outlook.com>
2017-03-26 19:27:36 +0200

    Improve serial communication
    Encapsulate serial connection
    Add a multitude of on event reactions

--- app/app.js
+++ app/app.js
@@ -7,32 +7,29 @@ import routes from './routes';
 import store from './store';
 import SerialPort from 'serialport';
 import { changeConnectAction, portAction } from './actions/settingActions';
-import { addPacket, bufferToPacket } from './helpers';
+import { addPacket, bufferToPacket, serialConnect } from './helpers';
 
 const routerHistory = syncHistoryWithStore(hashHistory, store);
 
 const rootElement = document.querySelector(document.currentScript.getAttribute('data-container'));
 
-/* serial port initialization */
+/* establishing serial connection */
 SerialPort.list((err, ports) => {
     if (err) throw err;
     if (store.getState().settings.autoconnect && ports.length === 1) {
         let com = ports[0].comName;
-        store.dispatch(portAction(com));
-        let port = new SerialPort(com, {
-            baudrate: 9600,
-            parser: SerialPort.parsers.raw
-        });
-        if(port) store.dispatch(changeConnectAction(true));
-        else throw new Error('Serial connection was not established');
-
-        port.on('data', (data) => {
+        serialConnect(com, 9800).on('data', (data) => {
             console.log(data);
-            data = bufferToPacket(data);
-            addPacket(data);
+            addPacket(bufferToPacket(data));
+        }).on('error', err => {
+            store.dispatch(changeConnectAction(false));
+            throw new Error(err);
+        }).on('disconnect', () => {
+            store.dispatch(changeConnectAction(false));
         });
     }
     else store.dispatch(changeConnectAction(false));
+    store.dispatch(portAction(ports));
 });
 
 ReactDOM.render(
